af48fa6efd93ef79c798a35ea72dde560962b64f
Mohamed Mayla <mohamed.mayla@gmail.com>
2017-08-05 14:16:15 +0200

    Add roboto font

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { jsonServerRestClient, Admin, Resource } from 'admin-on-rest';
 import restClient from './restClient';
 
+import Dashboard from './Dashboard';
 import { CarList } from './cars';
 import { StoreList } from './stores';
 
 const App = () => (
-  <Admin restClient={restClient('http://localhost:1337')}>
+  <Admin dashboard={Dashboard} restClient={restClient('http://localhost:1337')}>
   <Resource name="stores" list={StoreList} />
     <Resource name="cars" list={CarList} />
   </Admin>
---
+++ src/Dashboard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Card, CardHeader, CardText } from 'material-ui/Card';
+
+const Dashboard = () => (
+  <Card style={{ margin: '2em' }}>
+    <CardHeader title="Welcome to the administration" />
+    <CardText>Lorem ipsum sic dolor amet...</CardText>
+  </Card>
+);
+
+export default Dashboard;
