99f68227708f285a055791870ad0ccae4993443a
Mohamed Mayla <mohamed.mayla@gmail.com>
2017-08-05 18:44:36 +0200

    Handle login

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import { jsonServerRestClient, Admin, Resource } from 'admin-on-rest';
 import restClient from './restClient';
 
 import Dashboard from './Dashboard';
+import authClient from './authClient';
 import { CarList } from './cars';
 import { StoreList } from './stores';
 
 const App = () => (
-  <Admin dashboard={Dashboard} restClient={restClient('http://localhost:1337')}>
+  <Admin authClient={authClient} dashboard={Dashboard} restClient={restClient('http://localhost:1337')}>
   <Resource name="stores" list={StoreList} />
     <Resource name="cars" list={CarList} />
   </Admin>
---
+++ src/authClient.js
@@ -0,0 +1,46 @@
+import { AUTH_LOGIN, AUTH_LOGOUT, AUTH_ERROR, AUTH_CHECK } from 'admin-on-rest';
+import rp from 'request-promise';
+
+export default (type, params) => {
+    // called when the user attempts to log in
+    if (type === AUTH_LOGIN) {
+      const { username, password } = params;
+      const options = {
+        method: 'PUT',
+        uri: 'http://localhost:1337/login',
+        body: {
+          email: username,
+          password,
+        },
+        json: true,
+      };
+
+      return rp(options).then((json) => {
+        const username = json.username;
+        localStorage.setItem('username', username);
+      }).catch((err) => {
+        throw new Error(err);
+      });
+    }
+    return Promise.resolve();
+    
+    // called when the user clicks on the logout button
+    if (type === AUTH_LOGOUT) {
+        localStorage.removeItem('username');
+        return Promise.resolve();
+    }
+    // called when the API returns an error
+    if (type === AUTH_ERROR) {
+        const { status } = params;
+        if (status === 401 || status === 403) {
+            localStorage.removeItem('username');
+            return Promise.reject();
+        }
+        return Promise.resolve();
+    }
+    // called when the user navigates to a new location
+    if (type === AUTH_CHECK) {
+        return localStorage.getItem('username') ? Promise.resolve() : Promise.reject();
+    }
+    return Promise.reject('Unknown method');
+};
